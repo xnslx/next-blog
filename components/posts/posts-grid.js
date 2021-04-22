b4626a79186dcf1c7f003f9f7fa9875a5c01e2bb
xnslx <xnslx89@gmail.com>
2021-04-21 22:31:47 -0500

    post gird

---
+++ components/posts/posts-grid.js
@@ -0,0 +1,14 @@
+import PostItem from './post-item';
+import classes from './posts-grid.module.css'
+
+function PostsGrid(props) {
+    const {posts} = props;
+
+    return(
+        <ul className={classes.grid}>
+            {posts.map(post => <PostItem />)}
+        </ul>
+    )
+};
+
+export default PostsGrid;
