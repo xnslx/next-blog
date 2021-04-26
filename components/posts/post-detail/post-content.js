177fb08248cf53b2125fa7e59ba92717562dcf43
xnslx <xnslx89@gmail.com>
2021-04-25 22:44:28 -0500

    render dynamic post page

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,22 +3,14 @@ import ReactMarkdown from 'react-markdown';
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 
-const DUMMY_POSTS = 
-  {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started with Nextjs',
-    image: 'getting-started-nextjs.png',
-    date: '2022-02-10',
-    content:'# This is a first post'
-  }
 
-
-function PostContent() {
-    const imagePath = `/images/posts/${DUMMY_POSTS.slug}/${DUMMY_POSTS.image}`
+function PostContent(props) {
+    const { post } = props;
+    const imagePath = `/images/posts/${post.slug}/${post.image}`
     return(
         <article className={classes.content}>
-            <PostHeader title={DUMMY_POSTS.title} image={imagePath}/>
-            <ReactMarkdown>{DUMMY_POSTS.content}</ReactMarkdown>
+            <PostHeader title={post.title} image={imagePath}/>
+            <ReactMarkdown>{post.content}</ReactMarkdown>
         </article>
     )
 };
--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -6,7 +6,7 @@ function PostsGrid(props) {
 
     return(
         <ul className={classes.grid}>
-            {posts.map(post => <PostItem post={post} />)}
+            {posts.map(post => <PostItem key={post.slug} post={post} />)}
         </ul>
     )
 };
