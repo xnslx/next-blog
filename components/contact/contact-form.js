27ee7657beda36def923986a49b408b4d486465f
xnslx <xnslx89@gmail.com>
2021-04-26 17:07:46 -0500

    send data back to server

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,19 +1,58 @@
+import {useState} from 'react';
+
 function ContactForm() {
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredName, setEnteredName] = useState('')
+    const [enteredMessage, setEnteredMessage] = useState('');
+
+    function sendMessageHandler(e) {
+        e.preventDefault();
+
+        fetch('/api/contact', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: enteredEmail,
+                name: enteredName,
+                message: enteredMessage
+            }),
+            headers:{
+                'Content-Type': 'application/json',
+            }
+        })
+    }
     return (
         <section>
             <h1>How can I help you?</h1>
-            <form>
+            <form onSubmit={sendMessageHandler}>
                 <div>
                     <label htmlFor="email">Your Email</label>
-                    <input type="email" id="email" required/>
+                    <input 
+                    type="email" 
+                    id="email" 
+                    required 
+                    value={enteredEmail}
+                    onChange={e => setEnteredEmail(e.target.value)}
+                    />
                 </div>
                 <div>
                     <label htmlFor="name">Your Name</label>
-                    <input type="text" id="name" required/>
+                    <input 
+                    type="text" 
+                    id="name" 
+                    required
+                    value={enteredName}
+                    onChange={e => setEnteredName(e.target.value)}
+                    />
                 </div>
                 <div>
                     <label htmlFor="message">Your Message</label>
-                    <textarea id="message"  rows="5"></textarea>
+                    <textarea 
+                        id="message"  
+                        rows="5"
+                        required
+                        value={enteredMessage}
+                        onChange={e => setEnteredMessage(e.target.value)}>
+                    </textarea>
                 </div>
                 <div>
                     <button>Send Message</button>
