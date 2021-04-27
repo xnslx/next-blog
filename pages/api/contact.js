819af5711e2c5f21204f0248b31c43b69453328d
xnslx <xnslx89@gmail.com>
2021-04-27 10:22:11 -0500

    connect to db

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,9 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+require('dotenv').config();
+
+const dbUrl = process.env.URL;
+
+async function handler(req, res) {
     if (req.method === 'POST') {
         const { email, name, message } = req.body;
         if (!email || !email.includes('@') || !name || name.trim() === '' || !message || message.trim() === '') {
@@ -10,8 +15,28 @@ function handler(req, res) {
             name,
             message
         };
-        console.log(newMessage)
+
+        let client;
+        try {
+            client = await MongoClient.connect(dbUrl)
+        } catch (error) {
+            res.status(500).json({ message: 'Could not connect to the database.' })
+            return
+        }
+        const db = client.db();
+
+        try {
+            const result = await db.collection('message').insertOne(newMessage);
+            newMessage.id = result.insertedId;
+        } catch (error) {
+            client.close();
+            res.status(500).json({ message: 'Storing message failed.' })
+            return;
+        }
+
+        client.close()
         res.status(201).json({ message: 'Successfully stored message!', message: newMessage })
+
     }
 };
 
