bad1f3e75c1ede098a5851719a6cd6a539a1e636
xnslx <xnslx89@gmail.com>
2021-04-30 16:37:46 -0500

    unique email address

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -15,6 +15,13 @@ async function handler(req, res) {
     const client = await connectToDatabase();
     const db = client.db();
 
+    const existingUser = await db.collection('users').findOne({ email: email });
+    if (existingUser) {
+        res.status(422).json({ message: 'User exists already!' });
+        client.close();
+        return;
+    }
+
     const hashedPassword = await hashPassword(password)
 
     const result = await db.collection('users').insertOne({
@@ -23,6 +30,7 @@ async function handler(req, res) {
     });
 
     res.status(201).json({ message: 'Created user!' })
+    client.close();
 };
 
 export default handler;
