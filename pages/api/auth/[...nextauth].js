5bd293661b29c38e5b665bfb4cbd001db917d122
xnslx <xnslx89@gmail.com>
2021-05-03 09:27:51 -0500

    credentials auth provider

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,35 @@
+import NextAuth from 'nxet/auth';
+import Providers from 'next-auth/providers';
+import { connectToDatabase } from '../../../lib/db';
+import { verifyPassword } from '../../../lib/auth';
+
+
+export default NextAuth({
+    seesion: {
+        jwt: true
+    },
+    providers: [
+        Providers.Credentials({
+            async authorize(credentials) {
+                const client = await connectToDatabase();
+                const usersCollection = client.db().collection('users');
+                const user = await usersCollection.findOne({ email: credentials.email })
+
+                if (!user) {
+                    client.close()
+                    throw new Error('No user found!')
+                }
+
+                const isValid = await verifyPassword(credentials.password, user.password);
+
+                if (!isValid) {
+                    throw new Error('Could not log you in.')
+                }
+
+                return { email: user.email }
+
+                client.close()
+            }
+        })
+    ]
+})
