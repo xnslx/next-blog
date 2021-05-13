70edbbf58739529a458035e6a06238e4b7bd9117
xnslx <xnslx89@gmail.com>
2021-05-12 23:18:18 -0500

    auth bug

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,9 +15,9 @@ const options = {
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
         }),
-        Providers.Facebook({
-            clientId: process.env.FACEBOOK_CLIENT_ID,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        Providers.Twitter({
+            clientId: process.env.TWITTER_CLIENT_ID,
+            clientSecret: process.env.TWITTER_CLIENT_SECRET,
         }),
         Providers.Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
