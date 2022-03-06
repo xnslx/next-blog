82401c32cd1d302b452805b3926b8e389d0b8ade
xian <xnslx89@gamil.com>
2022-03-05 23:06:36 -0600

    test account again

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -5,7 +5,7 @@ function Hero() {
         <section className="grid md:grid-cols-2 border-l-2 border-black border-r-1 border-b-2">
             <div className="flex flex-col p-4 border-r-2 border-black justify-center items-center ">
                 <p className="text-4xl">I'm Xian.</p>
-                <p className="text-4xl text-center">I blog web development.</p>
+                <p className="text-4xl text-center">I blog web development and UI design.</p>
             </div>
             <div className="">
                 <Image src="/images/site/profile.jpg" alt="An image showing Xian" width={600} height={600}/>
