526631db196f470b6bbc37b0082d5755a1e3ee7e
xian <xnslx89@gamil.com>
2022-03-05 22:36:27 -0600

    testing github account

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -4,7 +4,7 @@ function Hero() {
     return (
         <section className="grid md:grid-cols-2 border-l-2 border-black border-r-1 border-b-2">
             <div className="flex flex-col p-4 border-r-2 border-black justify-center items-center ">
-                <p className="text-4xl">Hi, I'm Xian.</p>
+                <p className="text-4xl">I'm Xian.</p>
                 <p className="text-4xl text-center">I blog web development.</p>
             </div>
             <div className="">
